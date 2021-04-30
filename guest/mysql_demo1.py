65197852c3b597fd6ae18df6282d6e6f789bbb27
Accroid <470035388@qq.com>
2021-04-30 18:15:22 +0800

    新增发布会页面

--- guest/mysql_demo1.py
+++ guest/mysql_demo1.py
@@ -1,5 +1,4 @@
 #需要下载mysql，下载地址https://cdn.mysql.com/archives/mysql-installer/mysql-installer-community-5.5.27.2.msi
-
 import pymysql
 
 db = pymysql.connect(
@@ -8,9 +7,10 @@ db = pymysql.connect(
     user = 'root',
     password = 'admin',
     db='guest',
-    port= '3307',
-    charset='utf8mb4')
+    # port= '3306'
+    )
 
+#获取游标
 cur = db.cursor()
 sql_insert = 'INSERT INTO sign_guest(realname,phone,email,sign,event_id,create_time) VALUES ("tom",18800112233,"tom@mail.com",0,1,NOW());'
 
